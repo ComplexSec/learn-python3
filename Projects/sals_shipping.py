a3de2f47dd9509b48c51455378594d4b696820e7
ComplexSec <JPAKE200@caledonian.ac.uk>
2020-10-02 19:04:40 +0100

    Add files via upload

---
+++ Projects/sals_shipping.py
@@ -0,0 +1,52 @@
+def cost_of_ground(weight):
+  if (weight <= 2):
+    cost = 1.50
+  elif (weight <= 6 ):
+    cost = 3.00
+  elif (weight <= 10):
+    cost = 4.00
+  else:
+    cost = 4.75
+
+  return 20 + (cost * weight)
+
+print(cost_of_ground(8.4))
+
+shipping_cost_prem = 125.00
+
+def cost_of_drone(weight):
+  if (weight <= 2):
+    cost = 4.50
+  elif (weight <= 6 ):
+    cost = 9.00
+  elif (weight <= 10):
+    cost = 12.00
+  else:
+    cost = 14.25
+
+  return cost * weight
+
+print(cost_of_drone(1.5))
+
+def print_cheapest(weight):
+
+  ground = cost_of_ground(weight)
+  drone = shipping_cost_prem
+  premium = cost_of_drone(weight)
+
+  if ground < premium and ground < drone:
+    method = "standard ground"
+    cost = ground
+  elif preimum < ground and premium < drone:
+    method = "premium ground"
+    cost = premium
+  else:
+    method = "drone"
+    cost = drone
+
+  print("The cheapest option available is $%.2f with %s shipping"
+  % (cost, method)
+  )
+
+print_cheapest(4.8)
+print_cheapest(41.5)
